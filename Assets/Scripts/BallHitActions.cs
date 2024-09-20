82879c45e8ecbedc04ea7a7fe365759af1b9a235
Shob3r <andrewpinter23@gmail.com>
2024-09-20 12:57:30 -0700

    Add urp, Some sound effect systems

---
+++ Assets/Scripts/BallHitActions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BallHitActions : MonoBehaviour
+{
+    protected virtual void OnCollisionEnter(Collision other)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayAudioOnCollision.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayAudioOnCollision : BallHitActions
+{
+    public AudioClip soundEffect;
+    private AudioSource effectSource;
+
+    public bool playMultiple = false;
+
+    private byte timesHit = 0;
+
+    private void Start()
+    {
+        effectSource = GetComponent<AudioSource>();
+    }
+    
+    protected override void OnCollisionEnter(Collision other)
+    {
+        if (timesHit == 0 || playMultiple)
+        {
+            timesHit++;
+            effectSource.PlayOneShot(soundEffect);
+        }
+    }
+}
