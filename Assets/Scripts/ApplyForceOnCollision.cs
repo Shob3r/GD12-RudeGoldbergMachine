84ad4ed9b14ddfaf638170361723f3ee68109740
Shob3r <andrewpinter23@gmail.com>
2024-10-04 23:32:58 -0700

    code and scene cleanup, switch to new skybox

--- Assets/Scripts/ApplyForceOnCollision.cs
+++ Assets/Scripts/ApplyForceOnCollision.cs
@@ -5,16 +5,18 @@ public class ApplyForceOnCollision : ObjectCollisionActions
     public float targetAcceleration = 5.5f;
     protected override void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other.gameObject.name);
-        if (DoesColliderHaveRigidBody(other.gameObject))
+        GameObject collidedGameObject = other.gameObject;
+        if (DoesColliderHaveRigidBody(collidedGameObject))
         {
-            Rigidbody collidedRigidBody = other.gameObject.GetComponent<Rigidbody>();
+            Rigidbody collidedRigidBody = collidedGameObject.GetComponent<Rigidbody>();
             float forceRequired = collidedRigidBody.mass * targetAcceleration;
-            Debug.Log(forceRequired);
-            other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * forceRequired, ForceMode.Impulse);
-            return;
+            
+            collidedRigidBody.AddForce(Vector3.forward * forceRequired, ForceMode.Impulse);
         }
-        if(enableDebugMessages) Debug.LogWarning($"{other.gameObject.name} Does not have a RigidBody attached to it!");
+        else
+        {
+            if (enableDebugMessages) Debug.LogWarning($"{other.gameObject.name} Does not have a RigidBody attached to it!");
+        }
     }
 
     private bool DoesColliderHaveRigidBody(GameObject collidedObject)
